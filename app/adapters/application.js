3bedc9b4f70c86874d15a43107317b9a66209235
dondaum <sebastian.daum89@gmail.com>
2017-12-04 20:50:39 +0100

    first commit

---
+++ app/adapters/application.js
@@ -0,0 +1,19 @@
+import DS from 'ember-data';
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
+
+const { String: { pluralize, underscore } } = Ember;
+
+export default DS.JSONAPIAdapter.extend({
+
+  pathForType(type) {
+  return pluralize(underscore(type));
+  },
+
+  actions: {
+  blurBackground(blur) {
+    this.controllerFor('application').set('blur', blur);
+    }
+  }
+
+
+});
---
+++ app/components/book-cover.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+  actions: {
+
+    open() {
+      this.set('isShowingModal', true);
+      this.get('blurBackground')(true);
+    },
+
+    close() {
+      this.set('isShowingModal', false);
+      this.get('blurBackground')(false);
+    }
+  }
+
+});
